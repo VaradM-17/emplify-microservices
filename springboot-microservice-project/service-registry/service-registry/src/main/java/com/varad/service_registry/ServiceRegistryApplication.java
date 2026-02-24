ba6c95d4168945cce6b69271db286335db0cd2bf
Varad Mule <varadmule17@gmail.com>
2026-02-24 20:50:36 +0530

    Implemented Eureka Service Registry and registered microservices

---
+++ springboot-microservice-project/service-registry/service-registry/src/main/java/com/varad/service_registry/ServiceRegistryApplication.java
@@ -0,0 +1,20 @@
+package com.varad.service_registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(ServiceRegistryApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+		logger.info("Service Registry Application started successfully.");
+	}
+
+}
